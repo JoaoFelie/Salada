54ff1ce18bba5326f7fe2ead121d636f0fa13576
JoaoFelie <147654199+JoaoFelie@users.noreply.github.com>
2024-01-30 16:02:58 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+let saladafrutas = ["abacate", "maça", "goiaba"]
+
+console.log(saladafrutas);
+
+saladafrutas.pop();
+console.log(saladafrutas);
+
+saladafrutas.shift();
+console.log(saladafrutas);
+
+saladafrutas.push("banana");
+console.log(saladafrutas);
+
+saladafrutas.unshift("laranja");
+console.log(saladafrutas);
+
+saladafrutas.sort();
+console.log(saladafrutas);
